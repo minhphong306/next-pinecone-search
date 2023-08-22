2fc4b227eae4a5e61324c7dd52139b81e215b1ba
phongpt-qba <phongphan@qba.studio>
2023-08-22 20:39:59 +0700

    chore: improve LlamaEmbeddings

--- extends/langchain/embeddings/llama.ts
+++ extends/langchain/embeddings/llama.ts
@@ -1,32 +1,90 @@
 import axios from 'axios';
 import { Embeddings, EmbeddingsParams } from 'langchain/embeddings/base';
+import { CreateEmbeddingRequestInput } from 'openai';
+import { chunkArray } from '../util/chunk';
 
-export class LlamaEmbeddings extends Embeddings {
-  constructor(params?: EmbeddingsParams) {
-    super(params || {});
-  }
+export interface LlamaEmbeddingsParams {
+  /**
+   * The maximum number of documents to embed in a single request. This is
+   * limited by the OpenAI API to a maximum of 2048.
+   */
+  batchSize?: number;
 
-  async embedQuery(text: string) {
-    const result = await this.embedWithRetry(text);
+  /**
+   * Whether to strip new lines from the input text. This is recommended by
+   * OpenAI, but may not be suitable for all use cases.
+   */
+  stripNewLines?: boolean;
+}
 
-    return result.data[0].embedding;
-  }
+export class LlamaEmbeddings
+  extends Embeddings
+  implements LlamaEmbeddingsParams
+{
+  batchSize = 512;
+  stripNewLines = true;
 
-  async embedDocuments(documents: string[]) {
-    const embeddings: number[][] = [];
-
-    for (const document of documents) {
-      const result = await this.embedWithRetry(document);
-
-      embeddings.push(result.data[0].embedding);
+  constructor(
+    fields?: EmbeddingsParams & {
+      stripNewLines?: boolean;
+      batchSize?: number;
     }
+  ) {
+    const fieldsWithDefaults = { maxConcurrency: 2, ...fields };
 
+    super(fieldsWithDefaults);
+
+    this.stripNewLines = fields?.stripNewLines ?? this.stripNewLines;
+    this.batchSize = fields?.batchSize ?? this.batchSize;
+  }
+
+  /**
+   * Method to generate an embedding for a single document. Calls the
+   * embeddingWithRetry method with the document as the input.
+   * @param text Document to generate an embedding for.
+   * @returns Promise that resolves to an embedding for the document.
+   */
+  async embedQuery(text: string): Promise<number[]> {
+    const { data } = await this.embeddingWithRetry({
+      input: this.stripNewLines ? text.replace(/\n/g, ' ') : text,
+    });
+    return data.data[0].embedding;
+  }
+
+  /**
+   * Method to generate embeddings for an array of documents. Splits the
+   * documents into batches and makes requests to the OpenAI API to generate
+   * embeddings.
+   * @param texts Array of documents to generate embeddings for.
+   * @returns Promise that resolves to a 2D array of embeddings for each document.
+   */
+  async embedDocuments(texts: string[]): Promise<number[][]> {
+    const batches = chunkArray(
+      this.stripNewLines ? texts.map((t) => t.replace(/\n/g, ' ')) : texts,
+      this.batchSize
+    );
+
+    const batchRequests = batches.map((batch) =>
+      this.embeddingWithRetry({
+        input: batch,
+      })
+    );
+    const batchResponses = await Promise.all(batchRequests);
+
+    const embeddings: number[][] = [];
+    for (let i = 0; i < batchResponses.length; i += 1) {
+      const batch = batches[i];
+      const { data: batchResponse } = batchResponses[i];
+      for (let j = 0; j < batch.length; j += 1) {
+        embeddings.push(batchResponse.data[j].embedding);
+      }
+    }
     return embeddings;
   }
 
-  async embedWithRetry(input: string) {
-    try {
-      const result = await axios.post<{
+  async embeddingWithRetry(request: { input: CreateEmbeddingRequestInput }) {
+    return this.caller.call(
+      axios.post<{
         object: string;
         model: string;
         usage: {
@@ -37,12 +95,9 @@ export class LlamaEmbeddings extends Embeddings {
           object: string;
           embedding: number[];
         }[];
-      }>(`${process.env.LLAMA_SERVICE_ORIGIN}/v1/embeddings`, {
-        input,
-      });
-      return result.data;
-    } catch (error: any) {
-      throw error;
-    }
+      }>,
+      `${process.env.LLAMA_SERVICE_ORIGIN}/v1/embeddings`,
+      request
+    );
   }
 }
